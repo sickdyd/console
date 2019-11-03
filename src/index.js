23c23b4de1a1c4f18ae097925a612696b9e90595
sickdyd <roberto.reale.ja@gmail.com>
2019-11-03 21:52:07 +0900

    added polyfill for ie11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// These must be the first lines in src/index.js
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
